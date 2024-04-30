b764672ea5eeda4e4f4057f96bc38f3b94057fb6
MicDiMeo <162302605+PippinoCapriii@users.noreply.github.com>
2024-04-30 10:19:42 +0200

    Update storage.sql
    aggiunta l'inizializzazione dei percorsi

--- progettoKTM/db/storage.sql
+++ progettoKTM/db/storage.sql
@@ -14,8 +14,8 @@ CREATE TABLE product (
   image VARCHAR(20)
 );
 
-INSERT INTO product values (1,"felpa","falsa",20,5,"gucci");
-INSERT INTO product values (2,"felpa","falsa",15,13,"nike");
-INSERT INTO product values (3,"t-shirt","falsa",9,4,"gucci");
-INSERT INTO product values (4,"scarpa","falsa",30,11,"luis-vuittone");
-INSERT INTO product values (5,"pantalone","falsa",10,3,"dior");
+INSERT INTO product values (1,"felpa","falsa",20,5,"gucci","");
+INSERT INTO product values (2,"felpa","falsa",15,13,"nike","");
+INSERT INTO product values (3,"t-shirt","falsa",9,4,"gucci","");
+INSERT INTO product values (4,"scarpa","falsa",30,11,"luis-vuittone","");
+INSERT INTO product values (5,"pantalone","falsa",10,3,"dior","");
