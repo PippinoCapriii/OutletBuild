3623fed901cba641b1bb170a37c8a8474fb7c58a
MicDiMeo <162302605+PippinoCapriii@users.noreply.github.com>
2024-04-30 10:40:10 +0200

    Update storage.sql

--- OutletDream/db/storage.sql
+++ OutletDream/db/storage.sql
@@ -11,11 +11,11 @@ CREATE TABLE product (
   price int default 0,
   quantity int default 0,
   marca char(20) not null,
-  image VARCHAR(20)
+  image VARCHAR(512)
 );
 
-INSERT INTO product values (1,"felpa","falsa",20,5,"gucci");
-INSERT INTO product values (2,"felpa","falsa",15,13,"nike");
-INSERT INTO product values (3,"t-shirt","falsa",9,4,"gucci");
-INSERT INTO product values (4,"scarpa","falsa",30,11,"luis-vuittone");
-INSERT INTO product values (5,"pantalone","falsa",10,3,"dior");
+INSERT INTO product values (1,"felpa","falsa",20,5,"gucci","http://localhost:8080/StorageSession/imgs/catalogo/");
+INSERT INTO product values (2,"felpa","falsa",15,13,"nike","http://localhost:8080/StorageSession/imgs/catalogo/");
+INSERT INTO product values (3,"t-shirt","falsa",9,4,"gucci","http://localhost:8080/StorageSession/imgs/catalogo/");
+INSERT INTO product values (4,"scarpa","falsa",30,11,"luis-vuittone","http://localhost:8080/StorageSession/imgs/catalogo/");
+INSERT INTO product values (5,"pantalone","falsa",10,3,"dior","http://localhost:8080/StorageSession/imgs/catalogo/");
