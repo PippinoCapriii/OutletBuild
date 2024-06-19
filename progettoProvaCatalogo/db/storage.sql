d94d3dada312a60491359758e57fde87620819df
kabom120 <167976753+kabom120@users.noreply.github.com>
2024-06-19 11:50:27 +0200

    Delete progettoProvaCatalogo directory

--- progettoProvaCatalogo/db/storage.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO product values (1,"felpa","falsa",20,5,"gucci","");
-INSERT INTO product values (2,"felpa","falsa",15,13,"nike","");
-INSERT INTO product values (3,"t-shirt","falsa",9,4,"gucci","");
-INSERT INTO product values (4,"scarpa","falsa",30,11,"luis-vuittone","");
-INSERT INTO product values (5,"pantalone","falsa",10,3,"dior","");
